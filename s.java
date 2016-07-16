7fab69cea8d86106ee14e57a266d2588f514e8d5
priyankakannan <priyakannan1811@gmail.com>
2016-07-16 15:46:48 +0530

    Add files via upload

---
+++ s.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class s {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); 
+      int n=sc.nextInt();
+      if(n<0)
+
+      { System.out.println("negative");
+    }else
+            System.out.println("postive");
+}}
