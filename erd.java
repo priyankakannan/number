f579f9934b4eeb8e0925cfba328e87690f1c65f5
priyankakannan <priyakannan1811@gmail.com>
2016-07-16 15:57:11 +0530

    Add files via upload

---
+++ erd.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class erd {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in); 
+      int n=sc.nextInt();
+      int fact=1;
+      for(int i=1;i<n;i++)
+      {
+          fact=fact*i;
+          }
+        System.out.println("factorial is"+a+"is"+fact);}
+    
+}
