07e5e08e30de7c96dc9b833eca9e4ccde56fa480
priyankakannan <priyakannan1811@gmail.com>
2016-07-16 16:02:05 +0530

    Add files via upload

---
+++ rr.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class rr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); 
+      String n=sc.nextLine();
+      n=new StringBuffer(n).reverse().toString();
+        System.out.println(n);
+    }
+}
