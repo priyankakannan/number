c24554619431756a4b1a603c594db1c649879309
priyankakannan <priyakannan1811@gmail.com>
2016-07-21 12:33:04 +0530

    Add files via upload

---
+++ hel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class hel {
+    public static void main(String[] args) {
+        String s="Hello";
+        int n=0;
+        while(n<5){
+            System.out.println("Hello");
+            n++;
+        
+    }}
+    
+}
