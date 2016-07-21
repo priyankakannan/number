bcfde4a9c246db32525816e015a85548c094a6a3
priyankakannan <priyakannan1811@gmail.com>
2016-07-21 12:08:26 +0530

    Add files via upload

---
+++ fp.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class fp {
+    public static void main(String[] args) {
+        int i,nm,q,zi = 0;
+        Scanner sc=new Scanner(System.in);
+        nm=sc.nextInt();
+        q=sc.nextInt();
+        for(i=0;i<q;i++){
+           zi=nm*nm; 
+        }
+        System.out.println(zi);
+        
+    }
+}
