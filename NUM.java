c45cc303b3019428de51ad1e36aa51bc252f9535
priyankakannan <priyakannan1811@gmail.com>
2016-07-21 12:44:56 +0530

    Add files via upload

---
+++ NUM.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class NUM {
+     public static void main(String[] args) {
+        Scanner s1=new Scanner(System.in);
+        int a=s1.nextInt();
+        int b=s1.nextInt();
+         System.out.println("power of"+a+"is"+Math.pow(a,b));
+    
+     }
+   }
