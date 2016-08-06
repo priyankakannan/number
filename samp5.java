56fd168c9486c45cecdefb0c7d62994d6379690e
priyankakannan <priyakannan1811@gmail.com>
2016-08-06 16:30:08 +0530

    Add files via upload

---
+++ samp5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class mod {
+
+	public static void main(String[] args) {
+		int a1,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a1=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The modulation is");
+		c=a1%b;
+		System.out.println(c);
+
+	}
+
+}
