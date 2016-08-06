2932980d990bc8ac2abd03c58b545c9e9b81840d
priyankakannan <priyakannan1811@gmail.com>
2016-08-06 17:07:59 +0530

    Add files via upload

---
+++ samp11.java
@@ -0,0 +1,19 @@
+package gem;
+import java.util.Scanner;
+
+public class Apple {
+public static void main(String[] args, int i) {
+String sc="hello";
+for( i=0;i<=sc.length();i++)
+{
+    char e=sc.charAt(i);
+    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+        {
+String h=sc.substring(0, i);
+
+String g=sc.substring(i+ i);
+s=h+"Z"+g;
+}}
+}
+    System.out.println("The result is="+s);
+}}
